326a2a7dc27bf89ed9e091ddb6c31a1cc08e014d
BaineGames <noah.baine@gmail.com>
2022-02-11 22:22:55 -0800

    dev

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -2,7 +2,7 @@
     import Divisions from "$lib/Divisions.svelte";
 </script>
 <div class='p-3'>
-<h1 class="text-2xl font-bold">Welcome to Capac Youth Baseball Leauge</h1>
+<h1 class="text-2xl font-bold">Welcomee to Capac Youth Baseball Leauge</h1>
 <p>The objective of CYBL should be to implant firmly in the children of the community the ideals of good sportsmanship, honesty, loyalty, courage and respect for authority, so that they may be well-adjusted, stronger and happier children and will grow to be good, decent, healthy and trustworthy citizens.</p>
 <p>CYBL organization is classified as a 501 (c) (3) not for profit organization</p>
 <p>CYBL Annually hosts an ALL-STAR Tournament at our sporting complex consisting of 9 baseball / softball diamonds</p>
