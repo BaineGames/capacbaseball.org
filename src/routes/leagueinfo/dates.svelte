1b745cac5fed583244fa6b3b2e0b508de9b335fd
BaineGames <noah.baine@gmail.com>
2022-02-14 10:41:38 -0800

    dev

--- src/routes/leagueinfo/dates.svelte
+++ src/routes/leagueinfo/dates.svelte
@@ -4,14 +4,14 @@
 <div class='p-3'>
 	<h2 class="font-bold text-xl text-center mb-3">Registration Dates for 2022</h2>
 	<ul class="text-center">
-		<li>February 15th, 2022 at Capac Lions Club 6:00PM to 7:00PM</li>
-		<li>March 8th, 2022 at Capac Lions Club 6:00PM to 7:00PM</li>
-		<li>March 22nd, 2022 at Capac Lions Club 6:00PM to 7:00PM</li>
+		<li>February 15th, 2022 at Capac Lions Club 6PM-7PM</li>
+		<li>March 8th, 2022 at Capac Lions Club 6PM-7PM</li>
+		<li>March 22nd, 2022 at Capac Lions Club 6PM-7PM</li>
 	</ul>
 	<h3 class="font-bold text-xl text-center mb-3">Other Dates</h3>
 	<ul class="text-center mb-2">
 		<li>April 5th Coaches meeting and Team Drafts - Lions Hall</li>
-		<li>April 16th Equipment hand out 11:00AM</li>
+		<li>April 16th Equipment hand out 11AM</li>
 		<li>April 18th Practice can begin</li>
 		<li>May 7th Field work day</li>
 		<li>May 21st Opening day</li>
