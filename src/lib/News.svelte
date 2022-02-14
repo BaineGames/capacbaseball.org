74a20de76a657183d345b661126f0875df2b9878
BaineGames <noah.baine@gmail.com>
2022-02-14 10:42:54 -0800

    dev

--- src/lib/News.svelte
+++ src/lib/News.svelte
@@ -1,6 +1,6 @@
 <div class='bg-slate-100 h-full p-3'>
     <h2 class='text-xl font-bold'>News & Social</h2>
-    <p class='mt-5'>Special thank you to all our sponsors and volunteers for a successful season you make all this possible!</p>
+    <p class='mt-5'>Special thank you to all our sponsors and volunteers for a successful season. You make all this possible!</p>
     <p class='mt-5'>Be sure to follow our <a class='underline text-blue-600 visited:text-purple-600' href='https://www.facebook.com/capacyouthbaseball' rel='external' target='_blank'>Facebook page</a> for latest information</p>
     <p class='mt-5'>For rainout information, please contact the designated coach</p>
 </div>
