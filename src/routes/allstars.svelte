b2cc4f7cb481c433ce70a90cce42ff1203e03544
BaineGames <noah.baine@gmail.com>
2022-02-14 10:44:14 -0800

    dev

--- src/routes/allstars.svelte
+++ src/routes/allstars.svelte
@@ -2,24 +2,24 @@
 	<title>CYBL Allstars Information</title>
 </svelte:head>
 
-<div class='p-3 text-center'>
+<div class='p-3'>
 	<h2 class="font-bold text-xl">
 		2022 Capac Youth Baseball League All-Star Baseball / Softball Tournament
 	</h2>
-	<p>July 15th - 17th</p>
+	<p class='py-2'>July 15th - 17th</p>
 	<p class='w-full bg-yellow-300 p-3'>MHSAA Registered Umpires on every Field</p>
-	<p>Trophies will be awarded for 1st &amp; 2nd Place Teams. (Only 12 TROPHIES per Team)</p>
-	<p>Concession Stand will be available</p>
-	<p>
+	<p class='py-2'>Trophies will be awarded for 1st &amp; 2nd Place Teams. (Only 12 TROPHIES per Team)</p>
+	<p class='py-2'>Concession Stand will be available</p>
+	<p class='py-2'>
 		Rules &amp; Team Roster Sheets will be sent out upon receipt of entry fee to confirm your
 		registration
 	</p>
-	<p>
+	<p class='py-2'>
 		Check in at registration table 30 minutes prior to your first game with Team Roster and Birth
 		Certificates
 	</p>
-	<p>No Alcohol or Tobacco is allowed on School property</p>
-	<p>No Animals allowed on School property</p>
-	<p>No Camping allowed on School property</p>
+	<p class='py-2'>No Alcohol or Tobacco is allowed on School property</p>
+	<p class='py-2'>No Animals allowed on School property</p>
+	<p class='py-2'>No Camping allowed on School property</p>
     <p class='p-3'><a class='underline text-blue-600 visited:text-purple-600' href='/forms/allstarform.pdf' rel='external' target='_blank'>Download the all-star form here</a></p>
 </div>
